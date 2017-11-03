508bbfc647421b127b9948c3803b228688b8c8d7
Mathieu Agopian <mathieu@agopian.info>
2017-11-03 11:42:49 +0100

    Add another method: can_hold

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -9,6 +9,10 @@ impl Rectangle {
     fn area(&self) -> usize {
         self.width * self.height
     }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
 }
 
 fn main() {
@@ -25,6 +29,31 @@ fn main() {
         "Failed to read line",
     );
     let height = height.trim().parse().expect("That's not a number!");
+    let rect1 = Rectangle { width, height };
 
-    println!("The area is: {}", Rectangle { width, height }.area());
+    println!("The area is: {}", rect1.area());
+
+
+    println!("Enter the width of a second rectangle:");
+    let mut width2 = String::new();
+    io::stdin().read_line(&mut width2).expect(
+        "Failed to read line",
+    );
+    let width2 = width2.trim().parse().expect("That's not a number!");
+
+    println!("Enter the height of a second rectangle:");
+    let mut height2 = String::new();
+    io::stdin().read_line(&mut height2).expect(
+        "Failed to read line",
+    );
+    let height2 = height2.trim().parse().expect("That's not a number!");
+    let rect2 = Rectangle {
+        width: width2,
+        height: height2,
+    };
+
+    println!(
+        "The second rectangle fits in the first one: {}",
+        rect1.can_hold(&rect2)
+    );
 }
