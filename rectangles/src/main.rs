f01b1cbcc2038774bf42dd4826f2a9ea4be701e4
Mathieu Agopian <mathieu@agopian.info>
2017-11-03 11:14:46 +0100

    Refactor to use a Rectangle struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,5 +1,10 @@
 use std::io;
 
+struct Rectangle {
+    width: usize,
+    height: usize,
+}
+
 fn main() {
     println!("Enter the width of the rectangle:");
     let mut width = String::new();
@@ -15,9 +20,9 @@ fn main() {
     );
     let height = height.trim().parse().expect("That's not a number!");
 
-    println!("The area is: {}", area(width, height));
+    println!("The area is: {}", area(Rectangle { width, height }));
 }
 
-fn area(width: usize, height: usize) -> usize {
-    width * height
+fn area(rect: Rectangle) -> usize {
+    rect.width * rect.height
 }
