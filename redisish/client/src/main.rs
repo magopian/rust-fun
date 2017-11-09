6c027c5cb49b8f16b5b8089c28d72d26a1bac7ab
Mathieu Agopian <mathieu@agopian.info>
2017-11-09 15:26:21 +0100

    Get rid of all the unwrap() calls

--- redisish/client/src/main.rs
+++ redisish/client/src/main.rs
@@ -5,7 +5,7 @@ use std::io::{BufRead, Write, stdin};
 use bufstream::BufStream;
 
 fn main() {
-    let stream = TcpStream::connect("127.0.0.1:8000").unwrap();
+    let stream = TcpStream::connect("127.0.0.1:8000").expect("Couldn't connect to server");
     let mut stream = BufStream::new(stream);
 
     loop {
@@ -15,12 +15,23 @@ fn main() {
             "Failed to read line",
         );
 
-        stream.write(command.as_bytes());
-        stream.write(String::from("\n").as_bytes());
-        stream.flush();
-
-        let mut response = String::new();
-        stream.read_line(&mut response);
-        println!("Response: {:?}", response.trim());
+        let response = send_and_receive(&mut stream, command).expect("IO error");
+        if response.len() == 0 {
+            println!("Got nothing from the server, disconnected?");
+            break;
+        }
+        println!("Response: {:?}", response);
     }
+}
+
+fn send_and_receive(
+    stream: &mut BufStream<TcpStream>,
+    command: String,
+) -> Result<String, std::io::Error> {
+    stream.write(command.as_bytes())?;
+    stream.flush()?;
+
+    let mut response = String::new();
+    stream.read_line(&mut response)?;
+    Ok(response.trim().into())
 }
--- redisish/server/src/main.rs
+++ redisish/server/src/main.rs
@@ -12,7 +12,7 @@
 /// - use multile modules (multiple files preferably to lean import semantics better)
 /// - use an enum type for messages
 /// - make it threaded
-/// - multiple (named) channels (hashmap)
+/// - multiple (named) channels (hashmap? `std::collections::hash_map::Entry`)
 /// - remove all `unwrap` calls
 /// - use the `?` operator with your own error type (enum is fine for now)
 /// - use `&'a str` for your message parsing function (or: get rid of `String::from` where
@@ -32,62 +32,72 @@ struct Redisish {
 }
 
 impl Redisish {
-    fn handle_client(&mut self, mut stream: TcpStream) {
+    fn handle_client(&mut self, stream: TcpStream) -> Result<(), std::io::Error> {
         let mut stream = BufStream::new(stream);
 
         loop {
             let mut content = String::new();
             let content_len = stream.read_line(&mut content);
-            if content_len.unwrap() == 0 {
-                println!("Client disconnected, closing");
-                break;
-            } else {
-                println!("Content: {}", content);
-            }
+            match content_len {
+                Ok(len) => {
+                    if len == 0 {
+                        println!("Client disconnected, closing");
+                        return Ok(());
+                    } else {
+                        println!("Content: {}", content);
+                    }
+                }
+                Err(e) => return Err(e),
+            };
 
             if content == String::from("GET\n") {
-                self.get(&mut stream);
+                self.get(&mut stream)?;
             } else if content.starts_with("PUT ") {
                 let splitted: Vec<&str> = content.trim().split(" ").collect();
                 if splitted.len() >= 2 {
                     println!("Putting: {}", splitted[1]);
                     self.messages.push_front(splitted[1].to_string());
-                    stream.write(String::from("ACK\n").as_bytes());
+                    stream.write(String::from("ACK\n").as_bytes())?;
                 } else {
                     println!("Malformed PUT");
-                    stream.write(String::from("Malformed PUT, expecting 'PUT <some string>'\n").as_bytes());
+                    stream.write(
+                        String::from(
+                            "Malformed PUT, expecting 'PUT <some string>'\n",
+                        ).as_bytes(),
+                    )?;
                 }
             } else {
                 println!("Neither GETing nor PUTing...");
-                    stream.write(String::from("ACK\n").as_bytes());
+                stream.write(String::from("ACK\n").as_bytes())?;
             }
-            stream.flush();
+            stream.flush()?;
 
             println!("New queue length: {}", self.messages.len());
         }
     }
 
-    fn get(&mut self, stream: &mut BufStream<TcpStream>) {
+    fn get(&mut self, stream: &mut BufStream<TcpStream>) -> Result<(), std::io::Error> {
         match self.messages.pop_back() {
             Some(message) => {
                 println!("popped: {}", message);
-                stream.write(message.as_bytes());
-            },
+                stream.write(message.as_bytes())?;
+            }
             None => {
                 println!("Nothing left!");
-                stream.write(String::from("Nothing left!").as_bytes());
+                stream.write(String::from("Nothing left!").as_bytes())?;
             }
         }
-        stream.write(String::from("\n").as_bytes());
+        stream.write(String::from("\n").as_bytes())?;
+        Ok(())
     }
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Couldn't open listening socket");
 
     let mut messages = VecDeque::new();
     messages.push_front(String::from("foobar"));
-    let mut redisish = Redisish { messages : messages};
+    let mut redisish = Redisish { messages: messages };
     println!("Started the redis server");
 
     // accept connections and process them serially
@@ -95,9 +105,9 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("New client");
-                redisish.handle_client(stream);
-            },
-            Err(e) => println!("Error: {:?}", e)
+                redisish.handle_client(stream).expect("IO error");
+            }
+            Err(e) => println!("Error: {:?}", e),
         }
     }
 }
