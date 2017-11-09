2250c8eb32f42dcec907b05dc9ffefaa83e1e782
Mathieu Agopian <mathieu@agopian.info>
2017-11-09 12:34:51 +0100

    Don't disconnect server connection until client disconnects

--- redisish/server/src/main.rs
+++ redisish/server/src/main.rs
@@ -21,27 +21,40 @@ struct Redisish {
 impl Redisish {
     fn handle_client(&mut self, mut stream: TcpStream) {
         let mut stream = BufStream::new(stream);
-        let mut content = String::new();
-        stream.read_line(&mut content);
 
-        if content == String::from("GET\n") {
-            self.get(stream);
-        } else if content.starts_with("PUT ") {
-            let splitted: Vec<&str> = content.trim().split(" ").collect();
-            if splitted.len() >= 2 {
-                println!("Putting: {}", splitted[1]);
-                self.messages.push_front(splitted[1].to_string());
+        loop {
+            let mut content = String::new();
+            let content_len = stream.read_line(&mut content);
+            if content_len.unwrap() == 0 {
+                println!("Client disconnected, closing");
+                break;
             } else {
-                println!("Malformed PUT");
+                println!("Content: {}", content);
             }
-        } else {
-            println!("Neither GETing nor PUTing...");
-        }
 
-        println!("New queue length: {}", self.messages.len());
+            if content == String::from("GET\n") {
+                self.get(&mut stream);
+            } else if content.starts_with("PUT ") {
+                let splitted: Vec<&str> = content.trim().split(" ").collect();
+                if splitted.len() >= 2 {
+                    println!("Putting: {}", splitted[1]);
+                    self.messages.push_front(splitted[1].to_string());
+                    stream.write(String::from("ACK\n").as_bytes());
+                } else {
+                    println!("Malformed PUT");
+                    stream.write(String::from("Malformed PUT, expecting 'PUT <some string>'\n").as_bytes());
+                }
+            } else {
+                println!("Neither GETing nor PUTing...");
+                    stream.write(String::from("ACK\n").as_bytes());
+            }
+            stream.flush();
+
+            println!("New queue length: {}", self.messages.len());
+        }
     }
 
-    fn get(&mut self, mut stream: BufStream<TcpStream>) {
+    fn get(&mut self, stream: &mut BufStream<TcpStream>) {
         match self.messages.pop_back() {
             Some(message) => {
                 println!("popped: {}", message);
@@ -53,7 +66,6 @@ impl Redisish {
             }
         }
         stream.write(String::from("\n").as_bytes());
-        stream.flush();
     }
 }
 
@@ -68,7 +80,10 @@ fn main() {
     // accept connections and process them serially
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => redisish.handle_client(stream),
+            Ok(stream) => {
+                println!("New client");
+                redisish.handle_client(stream);
+            },
             Err(e) => println!("Error: {:?}", e)
         }
     }
